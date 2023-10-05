2bfe78e1ff7b4c1c3b33bfd5873f52dc05081918
UrielRbls <66899453+UrielRbls@users.noreply.github.com>
2023-10-04 18:37:31 -0600

    ejercicio codigo de otros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,17 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const n = document.querySelector('.name');
+const b = document.querySelector('.blog');
+const l = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {
+  n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  const data = await response.json ();
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+  n.textContent = '${data.name}';
+  b.textContent = '${data.blog}';
+  l.textContent = '${data.location}';
 }
 
 function handleError(err) {

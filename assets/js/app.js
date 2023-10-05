f235bea87b95348343b01f932040bd097de60cac
UrielRbls <66899453+UrielRbls@users.noreply.github.com>
2023-10-04 19:47:24 -0600

    actualizacion final de ejercicio

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,20 +4,23 @@ const n = document.querySelector('.name'); //Clase llamada en html, signo $ elim
 const b = document.querySelector('.blog'); //Se corrige nombre de variable de acuerdo a clase html
 const l = document.querySelector('.location'); //Variable corregida y clase creada en html
 
-async function displayUser(username) {
-  n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  const data = await response.json ();
-  console.log(data);
+async function displayUser(username) { //Funcion con el nombre de usuario
+  n.textContent = 'cargando...'; //Solicitud de cambio texto a variable 
+  const response = await fetch(`${usersEndpoint}/${username}`);//Solicitud para obtener informacion del usuario, con el nombre de usuario 
+  const data = await response.json ();//Data se utiliza para guardar la informacion que recaba del fetch y lo convierte a json
+  console.log(data);//imprime response como objeto json
   n.textContent = `${data.name}`;
   b.textContent = `${data.blog}`;
   l.textContent = `${data.location}`;
 }
 
-function handleError(err) {
+function handleError(err) { 
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  n.textContent = `Algo salió mal: ${err}`//Se cambian las comillas dobles por backtics
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski').catch(handleError); //Se llaman las 2 funciones, en caso que no funcione se despliega el error
+
+//El await se utiliza para esperar la respuesta de la pag para que el proceso pueda continuar
+//En conclusión, este codigo obtiene informacion de un usuario de github utilizando una API, dicha información se desplegara en una página HTML, en caso de no ser exitoso muestra errores para identificar lo que salga mal en el proceso.
